50c8253fdb0f6a3720cf80c5fb44502fd3d5de36
ataotao <tzvuf@163.com>
2020-06-12 22:53:52 +0800

    404路径跳转调整

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Redirect } from 'umi';
 
 export default function NoFound() {
-  return <Redirect to="/index" />;
+  return <Redirect to="/" />;
 }
---
+++ src/pages/p3/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Redirect } from 'umi';
+
+export default function index() {
+  return <Redirect to="/p2" />;
+}
